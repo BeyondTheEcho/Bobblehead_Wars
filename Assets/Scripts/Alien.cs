f444f4bb26e4512bf4a116c4240dfd1d80c042a7
Stelios Klapsis <Steliosklapsis@outlook.com>
2022-10-14 12:17:14 -0400

    Chapter 5 - Reduced Time Complexity Of Pathfinding

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -7,6 +7,8 @@ public class Alien : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent agent;
+    public float navigationUpdate;
+    private float navigationTime = 0;
 
 
     // Start is called before the first frame update
@@ -20,7 +22,12 @@ public class Alien : MonoBehaviour
     {
         if (target != null)
         {
-            agent.destination = target.position;
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
         }
     }
 }
