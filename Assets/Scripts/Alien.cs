c8f2eb6eb57a3e7f008d33956bb2fe6e3b04a0f4
Stelios Klapsis <Steliosklapsis@outlook.com>
2022-10-14 22:11:17 -0400

    Chapter 8 - Added Enemy Head Rolling On Death

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -10,6 +10,8 @@ public class Alien : MonoBehaviour
     public Transform target;
     public UnityEvent OnDestroy;
     public float navigationUpdate;
+    public Rigidbody head;
+    public bool isAlive = true;
 
     //Private Vars
     private NavMeshAgent agent;
@@ -25,27 +27,41 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
-        Die();
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
     public void Die()
     {
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
         OnDestroy.Invoke();
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
         Destroy(gameObject);
     }
 
